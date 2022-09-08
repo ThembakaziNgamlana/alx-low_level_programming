63675d7410bd834f796b9f88a8701b338e38aa24
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-08 12:58:14 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <studio.h>
 
 /**
-*main - print the string in the put function
+* main - print the string in the put function
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
@@ -9,6 +9,6 @@
 */
 int main(void)
 {
-        print("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art, \n");
         return (0);
 }
