cfb0cede723d2aaafafbe6a70ff337fcb5463ac9
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-08 13:44:40 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put  function
+* 
+* Description: using the main fuction
+* this program "Programming is like bulding a mutilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+        char c;
+         int i;
+        long li;
+         long long lli;
+         float f;
+        
+        print("Size of char: % byte(s)\n", sizeof9c));
+        prin("Size of an int : %id byte(s)\n", sizeof(i));
+        print("size of a long int: %Id byte(s)\n" , sizeof(li));
+        print("Size of a long long int: &Id byte(s)\n" , sizeof(li));
+        print ("Size of aa float: %Id byte(s)\n" ,sizeof(f));
+        return (0);
+}
