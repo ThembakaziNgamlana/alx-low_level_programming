f01fb8a2cb238c7faa2bf3b880054f3295f70183
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-09 01:25:14 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
+#incude <stdio.h>
 
 
-/**                                                                                                                                                                                  
+/**                                                                                                                                                                                   
 *main - print the string in the put function                                                                                                                                         
-*                                                                                                                                                                                     
-*Description: using the main function                                                                                                                                                
-*this program prints "Programing is like building a multilingual puzzle                                                                                                               
-* Return: 0                                                                                                                                                                           
-*/                                                                                                                                                                                    
-
+*                                                                                                                                                                                     *Description: using the main function                                                                                                                                                
+*this program prints "Programing is like building a multilingual puzzle
+*Return: 0                                                                                                              
+*/
 int main(void)                                                                                                                                                                        
-
-{                                                                                                                                                                                     
-  puts("\"Programming is like bulding a multilingual puzzle");                                                                                                                       
-  return (0);                                                                                                                                                                        
+{                                                                                                                                                                                      puts("\"Programming is like bulding a multilingual puzzle");                                                                                                                       
+ return(0);
 }
+
+
 
