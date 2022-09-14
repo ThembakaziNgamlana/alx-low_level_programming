fc7afb603d78a1f097251dfdc85246f011725abb
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-14 00:40:07 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - print putchar
  * Return: 0
@@ -9,14 +7,14 @@
 
 int main(void)
 {
-          _putchar('_');
+          putchar(");
           _putchar('p);
           _putchar('u');
           _putchar('t');
           _putchar('c');
           _putchar('h')
           _putchar('a')
-          _putchar('r")
+          _putchar('r')
           _putchar('\n');
       
            return (0);
