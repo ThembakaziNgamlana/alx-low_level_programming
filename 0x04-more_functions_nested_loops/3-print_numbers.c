d2fddf397181ec0797c311c89e627583fb69f001
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-16 15:07:55 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include 'main.h"
+
+/**
+*prnt_numbers - function that print  all numbers from 0 to 0
+*
+*Return: return void 
+*/
+
+void print_numbers(void)
+{
+int n;
+for (n = 48; n< 58; n++)
+{
+putchar(n);
+}
+
