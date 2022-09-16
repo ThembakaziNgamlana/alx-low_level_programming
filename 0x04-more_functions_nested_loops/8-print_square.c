140dc657df97549e1e4de0259e978cf88c680521
ThembakaziNgamlana <thembakzingamlana25@gmail.com>
2022-09-16 16:13:10 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_squre - print n squre according a number of tiomes
+ * @size: The number of squares/numbre of times
+ *
+ * Return: empty
+*/
+void print_squares(int size)
+{
+      int x, y;
+    
+      if (size <= 0)
+      {
+            _putchar('\n');
+      }
+      else 
+      {
+            for (x = 0; x < size; x++)
+            {
+                 for (y=0; y < size; y++)
+                            _putchar(35);
+            }
+                    _putchar('\n');
+            }
+       }
+}
